4f4b2e0d4968d30c38dbdbaf5472e95654632a8a
Alexandr <sasha732sasha000@gmail.com>
2019-02-26 21:53:32 +0200

    remove views, moved to 8080 port

--- src/config/express.ts
+++ src/config/express.ts
@@ -19,14 +19,10 @@ export default function (db) {
         }).catch(() => { console.log("Error connecting to mongos"); });
     }
 
-    app.set("views", path.join(__dirname, "../../src/views"));
-    app.set("view engine", "jade");
-
     app.use(logger("dev"));
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(cookieParser());
-    app.use(express.static(path.join(__dirname, "../../src/public")));
 
     for (let route of config.globFiles(config.routes)) {
         require(path.resolve(route)).default(app);
