2e741db68f321513ce0eb06cd551c68111fb6547
Alexandr <sasha732sasha000@gmail.com>
2019-02-27 23:36:09 +0200

    mongo connection string fix

--- src/config/config.ts
+++ src/config/config.ts
@@ -8,14 +8,14 @@ const getMongoConnectionString = () => {
 		case 'development':
 			return 'mongodb://localhost:27017/pickupme';
 		case 'production':
-			return 'mongodb://hZsBjny88GbwJyaw:67C2WTf748R2zeuDkJTn@127.0.0.1:27017/pickupme';
+			return 'mongodb://hZsBjny88GbwJyaw:67C2WTf748R2zeuDkJTn@127.0.0.1:27017/pickupme?authSource=admin';
 		default:
-			return 'mongodb://hZsBjny88GbwJyaw:67C2WTf748R2zeuDkJTn@127.0.0.1:27017/pickupme';
+			return 'mongodb://hZsBjny88GbwJyaw:67C2WTf748R2zeuDkJTn@165.227.157.101:27017/pickupme?authSource=admin';
 	}
 }
 
 export default class Config {
-	public static port: number = 555;
+	public static port: number = 8080;
 	public static routes: string = "./dist/routes/**/*.js";
 	public static models: string = "./dist/models/**/*.js";
 	public static useMongo: boolean = true;
