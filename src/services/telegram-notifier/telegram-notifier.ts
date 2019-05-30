7d23eae6d26b81781266913f2b88c957b663e657
Alexandr Dobrovolskiy <sasha732sasha000@gmail.com>
2019-05-30 13:06:26 +0300

    Telegram Notifier: driver contact name fix

--- src/services/telegram-notifier/telegram-notifier.ts
+++ src/services/telegram-notifier/telegram-notifier.ts
@@ -4,17 +4,18 @@ import fetch from 'node-fetch';
 export class TelegramNotifier {
   private static NOTIFY_URL: string = 'https://api.telegram.org/bot712762909:AAE79eaqD6woOXRiCXVH0tv6qRikOJOguXw';
 
-  private static composeUrls(phone: string, telegramId: string, name: string, ride: Ride): string[] {
-    const contact = `${this.NOTIFY_URL}/sendContact?chat_id=${telegramId}&phone_number=${phone}&first_name=${encodeURI(name)}`;
+  private static composeUrls(telegramId: string, ride: Ride): string[] {
+    const { driverContact } = ride;
+    const contact = `${this.NOTIFY_URL}/sendContact?chat_id=${telegramId}&phone_number=${driverContact.phone}&first_name=${encodeURI(driverContact.name)}`;
     const message = `${this.NOTIFY_URL}/sendMessage?chat_id=${telegramId}&text=${encodeURI(`Информация про поездку:\nКогда: ${new Date(ride.date).toLocaleDateString('uk-UA')},\nСтоимость поездки: ${ride.price} UAH.\n Мест в машине: ${ride.seats}.`)}`;
 
     return [contact, message];
   }
 
   private static notifyOne(ride: Ride, subscriber: any): Promise<Response> {
-    const { phone, telegramId, name } = subscriber;
+    const { telegramId } = subscriber;
 
-    const [contactUrl, messageUrl] = this.composeUrls(ride.driverContact.phone, telegramId, name, ride);
+    const [contactUrl, messageUrl] = this.composeUrls(telegramId, ride);
     return fetch(contactUrl)
       .then(() => fetch(messageUrl));
   }
